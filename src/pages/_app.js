37874cd81830a2bb1e6b1c2bc6c6e0d8a7147bb6
Islom <akhmedovaferuza72@gmail.com>
2024-12-28 11:31:45 +0500

    fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/bootstrap.css';
 import '../styles/global.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head';
-
-// import logo_text from './icons/logo_text.svg'
+import { Carousel } from 'react-responsive-carousel';
 
 export default function Home() {
+	const carousel_items = [
+		'ESTY',
+		'KINGSMAN',
+		'MAKRO',
+		'IT PARK',
+		'ФОРМА',
+		'ARKON',
+	]
+
 	return (
 		<div>
 			<Head>
@@ -38,13 +46,46 @@ export default function Home() {
 				</div>
 
 				<div className="right_card">
-
+					<img src="./images/carousel_1.png" alt="" className="w-100" />
 				</div>
 			</section>
 
-			<section className="block_2"></section>
-			<section className="block_3"></section>
-			<section className="block_4"></section>
+			<section className="block_2">
+				<div className="card">
+					<Carousel showArrows={true} >
+						{
+							carousel_items.map((item, index) => (
+								<div key={index}>
+									{item}
+								</div>
+							))}
+					</Carousel>
+				</div>
+			</section>
+
+			<section className="block_3">
+				<div className="card">
+					<p>
+						<b>Xeond</b> — это инновационная студия, которая объединяет искусство дизайна и мощь технологий для создания уникальных цифровых решений. Мы специализируемся на разработке игр, сайтов, приложений и сервисов, предоставляя полный цикл услуг — от концепции до готового продукта.
+						<br /><br />
+						Наша миссия — превращать идеи в качественные проекты, которые вдохновляют пользователей и обеспечивают нашим клиентам успех. Мы верим в силу индивидуального подхода, креативности и технологического мастерства, что позволяет нам находить идеальные решения даже для самых сложных задач.
+						<br /><br />
+						С Xeond ваш проект станет не просто функциональным, но и стильным, выделяющимся и запоминающимся.
+						Мы создаём не просто код — мы создаём впечатления.
+					</p>
+				</div>
+			</section>
+
+			<section className="block_4">
+				<div className="d-flex align-items-center">
+					<div className="card">
+
+					</div>
+					<div className="card"></div>
+					<div className="card"></div>
+					<div className="card"></div>
+				</div>
+			</section>
 			<section className="block_5"></section>
 			<section className="block_6"></section>
 			<section className="block_7"></section>
