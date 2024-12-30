b45bbd85e042e53f89780a994de4c5480cb6e8aa
Islom <akhmedovaferuza72@gmail.com>
2024-12-30 11:43:11 +0500

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,53 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 
 export default function Home() {
 	const carousel_items = [
 		'ESTY',
+		<div className="point" />,
 		'KINGSMAN',
+		<div className="point" />,
 		'MAKRO',
+		<div className="point" />,
 		'IT PARK',
+		<div className="point" />,
 		'ФОРМА',
+		<div className="point" />,
 		'ARKON',
+		<div className="point" />,
 	]
+
+	useEffect(() => {
+		// Проверяем, включено ли reduced motion
+		if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+			addAnimation();
+		}
+
+		function addAnimation() {
+			const scroller = document.querySelector(".scroller");
+
+			if (scroller) {
+				// Проверяем, было ли анимация уже добавлена
+				if (scroller.getAttribute("data-animated") === "true") {
+					return; // Если анимация уже настроена, выходим
+				}
+
+				// Устанавливаем атрибут data-animated
+				scroller.setAttribute("data-animated", true);
+
+				const scrollerInner = scroller.querySelector(".scroller__inner");
+				const scrollerContent = Array.from(scrollerInner.children);
+
+				// Дублируем элементы внутри контейнера
+				scrollerContent.forEach((item) => {
+					const duplicatedItem = item.cloneNode(true);
+					duplicatedItem.setAttribute("aria-hidden", true);
+					scrollerInner.appendChild(duplicatedItem);
+				});
+			}
+		}
+	}, []);
 
 	return (
 		<div>
@@ -51,15 +89,21 @@ export default function Home() {
 			</section>
 
 			<section className="block_2">
-				<div className="card">
-					<Carousel showArrows={true} >
-						{
-							carousel_items.map((item, index) => (
-								<div key={index}>
-									{item}
-								</div>
-							))}
-					</Carousel>
+				<div className="scroller" dataSpeed="fast">
+					<ul className="tag-list scroller__inner">
+						{carousel_items.map((item, index) => (
+							<li key={index} className="item">
+								{item}
+							</li>
+						))
+						}
+						{carousel_items.map((item, index) => (
+							<li key={`duplicate-${index}`} className="item">
+								{item}
+							</li>
+						))
+						}
+					</ul>
 				</div>
 			</section>
 
@@ -77,20 +121,112 @@ export default function Home() {
 			</section>
 
 			<section className="block_4">
-				<div className="d-flex align-items-center">
+				<div className="d-flex align-items-center gap-20">
 					<div className="card">
+						<p className="top">
+							Более 4х лет на рынке и  опыт в разных сферах
+						</p>
 
+						<p className="bottom">
+							4+
+						</p>
 					</div>
-					<div className="card"></div>
-					<div className="card"></div>
-					<div className="card"></div>
+
+					<div className="card">
+						<p className="top">
+							Множество успешных проектов
+						</p>
+
+						<p className="bottom">
+							65+
+						</p>
+					</div>
+
+					<div className="card">
+						<p className="top">
+							специалисты с опытом не по годам
+						</p>
+
+						<p className="bottom">
+							18+
+						</p>
+					</div>
+
+					<div className="card">
+						<p className="top">
+							в команду входят
+						</p>
+
+						<p className="bottom">
+							5+
+						</p>
+					</div>
 				</div>
 			</section>
+
 			<section className="block_5"></section>
-			<section className="block_6"></section>
-			<section className="block_7"></section>
+
+			<section className="block_6">
+				<div className="d-flex align-items-center gap-20">
+					<div className="card">
+						<p className="top">
+							Ux/ui Диз. -
+							Исламов Камрон
+						</p>
+
+						<img src='./images/specialist_1.png' alt='Ux/ui Дизайнер - Исламов Камрон' />
+					</div>
+
+					<div className="card">
+						<p className="top">
+							граф. диз. -
+							абдиев амир
+						</p>
+
+						<img src='./images/specialist_2.png' alt='Ux/ui Дизайнер - Исламов Камрон' />
+					</div>
+
+					<div className="card">
+						<p className="top">
+							Frontend dev -
+							латыпов артём
+						</p>
+
+						<img src='./images/specialist_3.png' alt='Ux/ui Дизайнер - Исламов Камрон' />
+					</div>
+
+					<div className="card">
+						<p className="top">
+							backend dev -
+							ислом ахмедов
+						</p>
+
+						<img src='./images/specialist_4.png' alt='Ux/ui Дизайнер - Исламов Камрон' />
+					</div>
+				</div>
+			</section>
+
+			<section className="block_7">
+				<div className="card">
+					<p>
+						присоединяйся
+						к <span className="text-primary">xeond</span> и оптимизируй
+						свои процессы
+					</p>
+
+					<div className="big-circles">
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</div>
+			</section>
+
 			<section className="block_8"></section>
+
 			<section className="block_9"></section>
+
 			<section className="block_10"></section>
 		</div>
 	);
